75eb7df7f18462972d2d4c6d5a79c581080a0db2
Alyxsis Salac <144504868+alySalac@users.noreply.github.com>
2024-04-18 21:26:51 +0800

    Create gif_widget.dart

---
+++ lib/widgets/gif_widget.dart
@@ -0,0 +1,10 @@
+gif_widget.dart
+// widgets/gif_widget.dart
+import 'package:flutter/material.dart';
+
+class GifWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('lib/assets/tired.gif');// Replace with actual path
+  }
+}
