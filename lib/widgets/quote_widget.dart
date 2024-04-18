17e600bda0e5b38bf0c7368df5fef10e11b79db8
Charmaine <144504941+bloodymaine@users.noreply.github.com>
2024-04-18 21:38:31 +0800

    Create quote_widget.dart

---
+++ lib/widgets/quote_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../services/quote_service.dart';
+
+class QuoteWidget extends StatefulWidget {
+  final QuoteService quoteService;
+
+  QuoteWidget({required this.quoteService});
+
+  @override
+  _QuoteWidgetState createState() => _QuoteWidgetState();
+}
+
+class _QuoteWidgetState extends State<QuoteWidget> {
+  late Future<String> _quoteFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _quoteFuture = widget.quoteService.fetchDailyQuote();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _quoteFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Failed to load quote');
+        } else {
+          return Center(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text(
+                snapshot.data.toString(),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
